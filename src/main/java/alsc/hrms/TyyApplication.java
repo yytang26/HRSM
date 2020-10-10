684acad8824ddba3807c65f67604ab271dcc6fd7
tyy <tyy@example.com>
2020-10-10 19:30:56 +0800

    all comments

---
+++ src/main/java/alsc/hrms/TyyApplication.java
@@ -0,0 +1,13 @@
+package alsc.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyyApplication.class, args);
+    }
+
+}
